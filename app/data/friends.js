1bf9a40e9025af4eab3c34fe44db1e57755d3532
Farid <farid.delapena@gmail.com>
2019-08-04 19:24:41 -0500

    finished project

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Josh",
+    "photo": "https://cdn1.edgedatg.com/aws/v2/abc/TheBachelorette/person/2448780/8d979307d2c6b072d5c133ea9feeac8c/1000x400-Q90_8d979307d2c6b072d5c133ea9feeac8c.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Sherri",
+        "photo": "http://62management.com/wp-content/uploads/2016/06/Jane-Ethridge-Viva-edit-04--721x1024.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+        "name": "Kirill",
+        "photo": "https://www.hoa.ox.ac.uk/sites/default/files/styles/person_profile_photo/public/hoa/images/person/john_blakinger_3.jpg?itok=vGGCNfsW",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+        "name": "Amy",
+        "photo": "https://m.media-amazon.com/images/M/MV5BOWFkZDVkNWQtNzNhZC00Zjk3LWIzMmEtNDgxNTkxZWI0MGE0XkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_SY1000_CR0,0,784,1000_AL_.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Gavin",
+    "photo" : "https://s-media-cache-ak0.pinimg.com/236x/e6/58/db/e658dbeb3518edeab6a19516b8146137.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+        "name": "Kayla",
+        "photo": "https://www.elheraldo.hn/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=5MsF9w3mbMz9xwnfb_dOXs$daE2N3K4ZzOUsqbU5sYvkHAE_ret72xLLjqAL6qHd6FB40xiOfUoExWL3M40tfzssyZqpeG_J0TFo7ZhRaDiHC9oxmioMlYVJD0A$3RbIiibgT65kY_CSDiCiUzvHvODrHApbd6ry6YGl5GGOZrs-&CONTENTTYPE=image/jpeg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "PJ",
+        "photo": "https://files.rcnradio.com/public/styles/image_834x569/public/2019-06/el_actor_juan_pablo_espinosa_0.jpg?itok=IJKd6I2H",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+        "name": "Janie",
+        "photo": "https://cdn.shopify.com/s/files/1/1571/2505/products/Janie_Privacy_Cloth_grande.jpg?v=1529429860",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+        "name": "Andre",
+        "photo": "https://www.hellomagazine.com/imagenes/celebrities/2019072775807/peter-andre-reveals-sad-loss-heartbreaking-statement/0-367-984/peter-andre-sad-loss-t.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Krissy",
+        "photo": "https://blogcms.gymshark.com/uploads/1516199673-gymshark-5-tips-for-meal-prep-krissy-blog-image172.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Zob",
+        "photo": "https://los40es00.epimg.net/los40/imagenes/2018/03/02/musica/1519995976_242292_1519996539_noticia_normal.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Danielle",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2bea-GAaQb-3sq0WiC4is6jjhhy1pqaQdbfPPLs7lSL9SEso8",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+        "name": "Charu",
+    "photo" : "https://s-media-cache-ak0.pinimg.com/736x/09/0f/e7/090fe7f53967e0a0da51f12fa147e01d.jpg",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Omar",
+        "photo": "https://www.hrw.org/sites/default/files/styles/square/public/multimedia_images_2016/omar_shakir_1.jpg?itok=QhRjwjO9",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
